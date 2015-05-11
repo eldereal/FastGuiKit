8a5fb00a03c2df83a30b36c437584c53d17871de
Yiyuan Bai <eldereal@gmail.com>
2015-05-11 23:02:55 +0800

    add UIActivityIndicatorView+FGStylable

---
+++ Helper/UIActivityIndicatorView+FGStylable.h
@@ -0,0 +1,14 @@
+//
+//  UIActivityIndicatorView+FGStylable.h
+//  exibitour
+//
+//  Created by 易元 白 on 15/5/11.
+//  Copyright (c) 2015年 cn.myzgstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FGStyle.h"
+
+@interface UIActivityIndicatorView (FGStylable) <FGStylable>
+
+@end
---
+++ Helper/UIActivityIndicatorView+FGStylable.m
@@ -0,0 +1,18 @@
+//
+//  UIActivityIndicatorView+FGStylable.m
+//  exibitour
+//
+//  Created by 易元 白 on 15/5/11.
+//  Copyright (c) 2015年 cn.myzgstudio. All rights reserved.
+//
+
+#import "UIActivityIndicatorView+FGStylable.h"
+
+@implementation UIActivityIndicatorView (FGStylable)
+
+- (void)styleWithColor:(UIColor *)color
+{
+    self.color = color;
+}
+
+@end
